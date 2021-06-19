dcc7bddd1764235ee4246b403504b9afa439b616
Hampton Moore <me@hampton.pw>
2021-06-18 22:18:16 -0400

    Remove NICK logging

--- modules/commands/NICK.js
+++ modules/commands/NICK.js
@@ -3,7 +3,6 @@ let NICK_C2S = ({socket, user, packet, network}) => {
     console.log(network.data.users)
     for (let uID in network.data.users){
         let u = network.data.users[uID];
-        console.log(u.data)
         if (u.data.NICK == packet.data.parameters[0]){
             socket.sendPacketFromServer({command: "433", parameters:[`* ${packet.data.parameters[0]} :Nickname is already in use`]});
             return;
